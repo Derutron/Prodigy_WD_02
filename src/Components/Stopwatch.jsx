15abf265e64f54316c3f3851b273695de41e8300
Derutron <fidg_1@yahoo.com>
2024-01-29 16:26:06 +0100

    Update Stopwatch.jsx

--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -39,7 +39,9 @@ const Stopwatch = () => {
   const handleLap = () => {
     if (startTime !== null) {
       const lapTime = Date.now() - startTime;
-      lapTimesRef.current.push(lapTime);
+      const previousLap = lapTimesRef.current[lapTimesRef.current.length - 1];
+      const difference = previousLap ? lapTime - previousLap.lapTime : 0;
+      lapTimesRef.current.push({ lapTime, difference });
     }
   };
 
@@ -57,60 +59,64 @@ const Stopwatch = () => {
 
   return (
     <div>
+      <div className="w-screen h-screen bg-blue-100 mx-auto p-4">
+        <h1 className='text-center my-10 font-[500] lg:text-6xl md:text-4xl sm:text-2xl '>STOP WATCH</h1>
+        <div className="text-center font-[500] mb-4 lg:text-4xl md:text-3xl sm:text-2xl">
+          {formatTime(elapsedTime)}
+        </div>
 
-    <div className="w-screen h-screen bg-blue-100 mx-auto p-4">
-    <h1 className='text-center my-10 font-[500] lg:text-6xl md:text-4xl sm:text-2xl '>STOP WATCH</h1>
-      <div className="text-center font-[500] mb-4 lg:text-4xl md:text-3xl sm:text-2xl">
-        {formatTime(elapsedTime)}
-      </div>
+        <div className="flex justify-center mb-4">
+          {!isRunning ? (
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-2 rounded"
+              onClick={handleStart}
+            >
+              Start
+            </button>
+          ) : (
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded"
+              onClick={handlePause}
+            >
+              Pause
+            </button>
+          )}
 
-      <div className="flex justify-center mb-4">
-        {!isRunning ? (
           <button
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-2 rounded"
-            onClick={handleStart}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
+            onClick={handleLap}
           >
-            Start
+            Lap
           </button>
-        ) : (
+
           <button
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded"
-            onClick={handlePause}
+            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 mx-2 rounded"
+            onClick={handleReset}
           >
-            Pause
+            Reset
           </button>
+        </div>
+
+        {lapTimesRef.current.length > 0 && (
+          <div>
+            <h2 className="text-center font-bold my-5 lg:text-3xl md:text-2xl sm:text-2xl">Lap Times:</h2>
+            <div className='flex justify-center'>
+              <ol className="list-decimal lg:text-3xl md:text-2xl sm:text-2xl font-[600] pl-8">
+                {lapTimesRef.current.map(({ lapTime, difference }, index) => (
+                  <li key={index}>
+                    {`Lap ${index + 1}: ${formatTime(lapTime)}`}
+                    {difference !== undefined && ` | (+${formatTime(difference)})`}
+                  </li>
+                ))}
+              </ol>
+            </div>
+          </div>
         )}
-
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
-          onClick={handleLap}
-        >
-          Lap
-        </button>
-
-        <button
-          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 mx-2 rounded"
-          onClick={handleReset}
-        >
-          Reset
-        </button>
       </div>
-
-      {lapTimesRef.current.length > 0 && (
-        <div>
-          <h2 className="text-center font-bold my-5 lg:text-3xl md:text-2xl sm:text-2xl">Lap Times:</h2>
-          <div className='flex justify-center'>
-          <ol className="list-decimal lg:text-3xl md:text-2xl sm:text-2xl font-[600] pl-8">
-            {lapTimesRef.current.map((lapTime, index) => (
-              <li key={index}>{formatTime(lapTime)}</li>
-            ))}
-          </ol>
-        </div> 
-         </div>
-      )}
-    </div>
     </div>
   );
 };
+
+
 
 export default Stopwatch;
